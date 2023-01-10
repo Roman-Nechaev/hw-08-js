e2ac84fa09fae693317cc024e17583e85fe51ab7
Roman-Nechaev <89315040+Roman-Nechaev@users.noreply.github.com>
2023-01-10 11:14:11 +0000

    Deploying to gh-pages from @ Roman-Nechaev/hw-08-js@cc6b2e7d8b0f39b5efc794cc54964e50ba276456 🚀

--- 02-video.a8ab9375.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),t=new Vimeo.Player(e);let r=localStorage.getItem("videoplayer-current-time");var o;(o=r)&&t.setCurrentTime(o),t.on("timeupdate",(function(e){localStorage.setItem("videoplayer-current-time",e.seconds)}));
-//# sourceMappingURL=02-video.a8ab9375.js.map
